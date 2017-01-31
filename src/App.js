12129a8c1c519e0914879400b44d3326cebe63a6
Bryan Wegman <xxbw@icloud.com>
2017-01-30 19:51:13 -0800

    Start over with custom-react-scripts

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//styles
+import './App.less';
+import styles from './Modules.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default App;
